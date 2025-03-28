745f47a1edbeaa726ad251d6c6aa759c2de9caf1
RobertoRG4 <robertoromeroguadarrama@gmail.com>
2025-03-27 21:45:07 -0600

    migrations

--- database/migrations/2025_03_28_031413_create_materials_table.php
+++ database/migrations/2025_03_28_031413_create_materials_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
             $table->string('name');
             $table->integer('quantity');
             $table->integer('stock');
+            $table->string('evidence');
             $table->timestamps();
         });
     }
