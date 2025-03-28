f61098598e63a41b1ef0287b9b24ab8019d92600
AlanAndres255 <perezlozanoalanandres@gmail.com>
2025-03-27 19:19:06 -0600

    migraciones

--- database/migrations/0001_01_01_000000_create_tables.php
+++ database/migrations/0001_01_01_000000_create_tables.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->foreignId('role_id')->constrained('roles');
@@ -33,25 +32,13 @@ return new class extends Migration
             $table->timestamps();
         });
 
-        Schema::create('order_statuses', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->timestamps();
-        });
-
-        Schema::create('invoices', function (Blueprint $table) {
-            $table->id();
-            $table->string('number')->unique();
-            $table->string('tax_details');
-            $table->timestamps();
-        });
-
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->foreignId('customer_id')->constrained('customers');
-            $table->foreignId('invoice_id')->nullable()->constrained('invoices');
+            $table->string('invoice_number')->nullable()->unique();
+            $table->string('tax_details')->nullable();
             $table->date('date');
-            $table->foreignId('status_id')->constrained('order_statuses');
+            $table->string('status');
             $table->timestamps();
         });
 
@@ -78,12 +65,6 @@ return new class extends Migration
             $table->timestamps();
         });
 
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
             $table->foreignId('user_id')->nullable()->index();
@@ -99,13 +80,10 @@ return new class extends Migration
         Schema::dropIfExists('users');
         Schema::dropIfExists('roles');
         Schema::dropIfExists('customers');
-        Schema::dropIfExists('order_statuses');
-        Schema::dropIfExists('invoices');
         Schema::dropIfExists('orders');
         Schema::dropIfExists('materials');
         Schema::dropIfExists('material_order');
         Schema::dropIfExists('evidences');
-        Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
     }
 };
