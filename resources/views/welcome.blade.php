20ea7a3e8fe16841e1e01918ddb0a48f1e376af9
GadGarciaa <gegg1704@gmail.com>
2025-05-09 09:15:15 -0600

    Updated welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,12 @@
-@extends('layouts.app')
+@extends('layout')
+
+@section('title', 'Welcome')
 
 @section('content')
-
-<h1>Welcome to Halcon</h1>
-<p>Welcome to the Halcon web application, this is a simple web application built with Laravel.</p>
-<p>Use the navigation links to explore different sections of the application.</p>
-@endsection 
+    <div class="text-center mt-5">
+        <h1 class="display-4">Welcome to Halcon</h1>
+        <p class="lead">This is a simple web application built with Laravel.</p>
+        <p>Use the navigation links above to explore different sections of the application.</p>
+        <a href="{{ route('orders.index') }}" class="btn btn-primary btn-lg mt-3">View Orders</a>
+    </div>
+@endsection
